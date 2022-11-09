89d4984a559c9b5420937c551e428ca8f4ef7fbc
Beanxx <tnqls991211@naver.com>
2022-11-09 21:08:02 +0900

    feat: Router 기본세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Main from "./pages/Main"
+import Post from "./pages/Post"
+import Header from "./components/templates/Header"
+import Login from "./pages/Login"
+import Join from "./pages/Join"
+import Mypage from "./pages/Mypage"
+import Users from "./pages/Users"
+import Detail from "./pages/Detail"
 
 function App() {
-  return <div>TS-Project</div>
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
---
+++ client/src/components/atoms/atoms.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const atoms = () => {
+  return <div>atoms</div>
+}
+
+export default atoms
