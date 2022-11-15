01bdc0bc81d2ee2049a56be043cfa6889dbcd708
mini <0miiii@naver.com>
2022-11-15 11:04:03 +0900

    feat: RelatedTags

---
+++ client/src/components/molecules/RelatedTags/RelatedTags.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Container = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  h3 {
+    margin: 16px 0;
+    font-size: 20px;
+  }
+`
+
+export const Num = styled.span`
+  color: rgb(106, 115, 124);
+  font-size: 12px;
+`
