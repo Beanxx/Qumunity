54d43ee6ab9627bd76e262913409e032404487cb
mini <0miiii@naver.com>
2022-11-15 13:07:11 +0900

    feat: miniHeader 추가

---
+++ client/src/components/molecules/MiniHeader/MiniHeader.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 12px;
+
+  h1 {
+    font-size: 27px;
+  }
+`
+
+export default Container
