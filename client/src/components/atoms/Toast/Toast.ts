2de9a6189b5dd8477948a2676e8719688dd2572b
Beanxx <tnqls991211@naver.com>
2022-11-19 19:34:53 +0900

    feat: Toast 컴포넌트 추가 - sweetAlert2 적용

---
+++ client/src/components/atoms/Toast/Toast.ts
@@ -0,0 +1,17 @@
+import Swal from "sweetalert2"
+import "./Toast.css"
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top",
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: true,
+  width: "370px",
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer)
+    toast.addEventListener("mouseleave", Swal.resumeTimer)
+  },
+})
+
+export default Toast
