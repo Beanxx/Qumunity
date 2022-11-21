df222bf53a3365edc2128a7e4989f7a06d97cc4b
Beanxx <tnqls991211@naver.com>
2022-11-22 00:23:54 +0900

    feat: 회원가입 client-server api 통신 구현

--- client/src/components/templates/JoinForm/JoinForm.tsx
+++ client/src/components/templates/JoinForm/JoinForm.tsx
@@ -1,29 +1,89 @@
 import React, { useState } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router"
 import LoginInput from "../../atoms/LoginInput"
 import Button from "../../atoms/Button"
 import Form from "../../../layouts/Form/Form"
 import * as S from "./styles"
+import Toast from "../../atoms/Toast/Toast"
 import { ReactComponent as QuestionImg } from "../../../assets/icons/questionmark.svg"
+import firebase from "../../../firebase-config"
 
 const JoinForm = () => {
-  const [nickname, setNickname] = useState("")
+  const navigate = useNavigate()
+
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [pw, setPw] = useState("")
   const [pwConfirm, setPwConfirm] = useState("")
-  const [errorMsg, setErrorMsg] = useState("")
+  const [flag, setFlag] = useState(false)
 
   const JoinFunc = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
+    setFlag(true)
     e.preventDefault()
+
+    if (!(name && email && pw && pwConfirm)) {
+      Toast.fire({
+        icon: "warning",
+        text: "모든 칸을 채워주세요.",
+      })
+    }
+
+    if (pw !== pwConfirm) {
+      Toast.fire({
+        icon: "warning",
+        text: "비밀번호와 비빌번호 확인 값은 같아야 합니다.",
+      })
+    }
+
+    const createdUser = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, pw)
+
+    await createdUser?.user?.updateProfile({
+      displayName: name,
+      // photoURL:
+      //   "https://kr.object.ncloudstorage.com/community-bucket/post/profile.png",
+    })
+
+    const body = {
+      email: createdUser?.user?.multiFactor.user.email,
+      displayName: createdUser?.user?.multiFactor.user.displayName,
+      uid: createdUser?.user?.multiFactor.user.uid,
+      // photoURL:
+      //   "https://kr.object.ncloudstorage.com/community-bucket/post/profile.png",
+    }
+
+    console.log(body)
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/api/user/join`, body)
+      .then((response) => {
+        setFlag(false) // 회원가입이 완료되면 다시 버튼 활성화해주기
+        if (response.data.success) {
+          Toast.fire({
+            icon: "success",
+            text: "Go!Street 회원가입을 축하드립니다 :>",
+          }).then(() => {
+            navigate("/login")
+          })
+        } else {
+          return Toast.fire({
+            icon: "error",
+            text: "회원가입에 실패하였습니다.",
+          })
+        }
+      })
   }
 
   return (
     <Form style={S.styles.container}>
       <LoginInput
         type="text"
-        value={nickname}
-        onChange={(e) => setNickname(e.currentTarget.value)}
+        value={name}
+        onChange={(e) => setName(e.currentTarget.value)}
       >
         NickName
       </LoginInput>
@@ -41,7 +101,6 @@ const JoinForm = () => {
       >
         Password
       </LoginInput>
-      {errorMsg !== "" && <p>{errorMsg}</p>}
       <LoginInput
         type="password"
         value={pwConfirm}
@@ -64,7 +123,11 @@ const JoinForm = () => {
         </div>
       </S.CheckDescription>
 
-      <Button btnType="highlighted" onClick={(e) => JoinFunc(e)}>
+      <Button
+        disabled={flag}
+        btnType="highlighted"
+        onClick={(e) => JoinFunc(e)}
+      >
         Sign up
       </Button>
 
