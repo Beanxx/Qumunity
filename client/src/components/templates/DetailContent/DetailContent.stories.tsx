fe6c90aac53ba627f985fa4e03a39341dcde6e15
mini <0miiii@naver.com>
2022-11-22 03:48:07 +0900

    feat: detail content story 수정

--- client/src/components/templates/DetailContent/DetailContent.stories.tsx
+++ client/src/components/templates/DetailContent/DetailContent.stories.tsx
@@ -1,18 +1,12 @@
 import React from "react"
 import { Story } from "@storybook/react"
-import DetailContent, { Props } from "./DetailContent"
+import DetailContent from "./DetailContent"
 
 export default {
   title: "templates/DetailContent",
   component: DetailContent,
 }
 
-const Template: Story<Props> = (args) => <DetailContent {...args} />
+const Template: Story = (args) => <DetailContent {...args} />
 
 export const Default = Template.bind({})
-
-Default.args = {
-  profileImg: `${process.env.PUBLIC_URL}/logo192.png`,
-  userName: "userName",
-  createdDate: "2022-11-10",
-}
