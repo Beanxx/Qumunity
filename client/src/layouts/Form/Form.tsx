cd1fc3f8032945e5edc7a663f76a2f773ebcdd43
Beanxx <tnqls991211@naver.com>
2022-11-23 01:50:03 +0900

    ✨ feat: Form 컴포넌트 type 추가

--- client/src/layouts/Form/Form.tsx
+++ client/src/layouts/Form/Form.tsx
@@ -4,12 +4,18 @@ import Container from "./styles"
 interface Props {
   children: React.ReactNode
   style?: object
+  id?: string
+  onSubmit?: () => void
 }
 
-const Form: React.FC<Props> = ({ children, style }) => {
+const Form: React.FC<Props> = ({ children, style, id, onSubmit }) => {
   const classes = style
 
-  return <Container style={classes}>{children}</Container>
+  return (
+    <Container style={classes} id={id} onSubmit={onSubmit}>
+      {children}
+    </Container>
+  )
 }
 
 export default Form
