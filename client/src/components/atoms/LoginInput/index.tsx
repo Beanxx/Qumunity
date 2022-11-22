438316b47977156a687030807990163fad3e1d8d
Beanxx <tnqls991211@naver.com>
2022-11-23 01:52:12 +0900

    ✨ feat: LoginInput - forwardRef & 타입 추가

--- client/src/components/atoms/LoginInput/index.tsx
+++ client/src/components/atoms/LoginInput/index.tsx
@@ -1,36 +1,42 @@
 import React from "react"
+import { UseFormRegisterReturn } from "react-hook-form"
 import * as S from "./styles"
 
-export type Props = {
+// export type Props = {
+//   children: React.ReactNode
+//   value: string
+//   type: string
+//   sub?: string
+//   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+// }
+
+interface InputType {
   children: React.ReactNode
-  value: string
-  type: string
+  id: string
+  type?: string
   sub?: string
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const LoginInput: React.FC<Props> = ({
-  children,
-  value,
-  type,
-  sub,
-  onChange,
-}) => {
+export interface Props extends InputType {
+  register?: UseFormRegisterReturn
+}
+
+const LoginInput = React.forwardRef((props: Props, ref) => {
   return (
     <S.Container>
       <S.Box>
-        <label htmlFor="title">{children}</label>
-        <span>{sub}</span>
+        <label htmlFor={props.id}>{props.children}</label>
+        <span>{props.sub}</span>
       </S.Box>
 
       <input
-        defaultValue={value}
-        onChange={onChange}
-        type={type}
+        id={props.id}
+        type={props.type}
         autoComplete="off"
+        {...props.register}
       />
     </S.Container>
   )
-}
+})
 
 export default LoginInput
