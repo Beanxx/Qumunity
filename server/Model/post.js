c59a74217ebe9ec4a8c3c791a0e2a9ca876e8f38
mini <0miiii@naver.com>
2022-11-24 13:50:37 +0900

    refactor: app.tsx 불필요코드삭제

--- server/Model/post.js
+++ server/Model/post.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   title: String,
   content: String,
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 module.exports = mongoose.model("post", PostSchema);
