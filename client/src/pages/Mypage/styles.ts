b7184b119e8fbb022f88f7ac1ad5b87399abbd77
Beanxx <tnqls991211@naver.com>
2022-11-25 01:32:10 +0900

    ✨ feat: Mypage 페이지 레이아웃

---
+++ client/src/pages/Mypage/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+const Layout = styled.div`
+  width: 100%;
+  height: calc(100vh - 50px);
+  background-color: #f1f2f3;
+  display: flex;
+  flex-direction: column;
+`
+export default Layout
