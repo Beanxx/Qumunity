8dfb7515adec1414e536091db0ca7d2b94c2180c
mini <0miiii@naver.com>
2022-11-25 18:24:02 +0900

    ✨ feat: server post model 수정

--- server/Model/post.js
+++ server/Model/post.js
@@ -5,9 +5,13 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   postNum: Number,
   title: String,
+  summury: String,
   content: String,
   tags: Array,
   createDate: { type: Date, default: Date.now },
+  votes: { type: Number, default: 0 },
+  answers: { type: Number, default: 0 },
+  views: { type: Number, default: 0 },
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
