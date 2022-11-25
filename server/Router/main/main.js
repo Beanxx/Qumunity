e6d19b660a5257f2b965b46e7ebf0520b3c3d3e6
mini <0miiii@naver.com>
2022-11-25 18:22:58 +0900

    ✨ feat: server main ctrl 추가

--- server/Router/main/main.js
+++ server/Router/main/main.js
@@ -7,6 +7,4 @@ const ctrl = require("./main.ctrl");
 router.get("/", ctrl.output.main);
 router.get("/tagSearch", ctrl.output.tagSearch);
 
-router.post("/test", ctrl.process.test);
-
 module.exports = router;
