1f7048a8410f9b3e4b660d91e5ef073116f64959
mini <0miiii@naver.com>
2022-11-25 19:19:53 +0900

    ✨ feat: server main ctrl res status 추가

--- server/Router/main/main.ctrl.js
+++ server/Router/main/main.ctrl.js
@@ -5,8 +5,8 @@ const Post = require("../../Model/post");
 const output = {
   main: async (req, res) => {
     try {
-      const postData = await Post.find().exec();
-      res.json(postData);
+      const postData = await Post.find().populate("author").exec();
+      res.status(200).json(postData);
     } catch (err) {
       res.status(400).json({ success: false, msg: err });
     }
