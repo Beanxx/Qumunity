fb78cb10df44db2eead3c9a2f85fe1cc7ffb2790
mini <0miiii@naver.com>
2022-11-26 10:26:15 +0900

    ✨ feat: server answer router 설정

--- server/Router/answer/answer.js
+++ server/Router/answer/answer.js
@@ -5,6 +5,6 @@ const router = express.Router();
 const ctrl = require("./answer.ctrl");
 
 router.get("/:id", ctrl.output.answer);
-router.post("/:id", ctrl.process.register);
+router.post("/register", ctrl.process.register);
 
 module.exports = router;
