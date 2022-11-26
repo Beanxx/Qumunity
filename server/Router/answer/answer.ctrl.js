8e7f2ca740e4e92d7459076842af027ffecf6654
mini <0miiii@naver.com>
2022-11-26 12:13:02 +0900

    ✨ feat: server answer ctrl get answer process

--- server/Router/answer/answer.ctrl.js
+++ server/Router/answer/answer.ctrl.js
@@ -4,11 +4,21 @@ const Answer = require("../../Model/answer");
 const Post = require("../../Model/post");
 const { User } = require("../../Model/User");
 
-const output = {
-  getAnswers: async (req, res) => {},
-};
+const output = {};
 
 const process = {
+  getAnswers: async (req, res) => {
+    Answer.find({ postId: req.body.postId })
+      .populate("author")
+      .exec()
+      .then((data) => {
+        return res.status(200).json({ success: true, answerList: data });
+      })
+      .catch((err) => {
+        return res.status(400).json({ success: false, msg: err });
+      });
+  },
+
   register: async (req, res) => {
     const answerData = new Answer(req.body);
     User.findOne({ uid: req.body.uid })
