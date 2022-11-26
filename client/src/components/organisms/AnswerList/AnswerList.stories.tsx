83ac94d93ce46323e29cc788a39d9239b23f91c9
mini <0miiii@naver.com>
2022-11-26 17:02:24 +0900

    ✨ feat: stories props edit

--- client/src/components/organisms/AnswerList/AnswerList.stories.tsx
+++ client/src/components/organisms/AnswerList/AnswerList.stories.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { Story } from "@storybook/react"
 import AnswerList from "./AnswerList"
-import postType from "../../../types/post.interface"
+import answerType from "../../../types/answer.interface"
 
 export default {
   title: "organisms/AnswerList",
   component: AnswerList,
 }
 
-const Template: Story<{ detailData: postType }> = (args) => (
+const Template: Story<{ answerData: answerType[] }> = (args) => (
   <AnswerList {...args} />
 )
 
