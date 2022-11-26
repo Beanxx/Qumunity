a6069651a9e00b86c5bf57741831d692f4da0e53
mini <0miiii@naver.com>
2022-11-26 10:26:44 +0900

    ✨ feat: answer ctrl register 생성

--- server/Router/answer/answer.ctrl.js
+++ server/Router/answer/answer.ctrl.js
@@ -1,13 +1,36 @@
 "use strict";
 
+const Answer = require("../../Model/answer");
 const Post = require("../../Model/post");
+const { User } = require("../../Model/User");
 
 const output = {
-  answer: async (req, res) => {},
+  getAnswers: async (req, res) => {},
 };
 
 const process = {
-  register: async (req, res) => {},
+  register: async (req, res) => {
+    const answerData = new Answer(req.body);
+    User.findOne({ uid: req.body.uid })
+      .exec()
+      .then((userInfo) => {
+        answerData.author = userInfo._id;
+
+        answerData.save(() => {
+          Post.findOneAndUpdate(
+            { _id: req.body.postId },
+            { $inc: { answers: 1 } }
+          )
+            .exec()
+            .then(() => {
+              res.status(200).json({ success: true });
+            });
+        });
+      })
+      .catch((err) => {
+        res.status(400).json({ success: false, msg: err });
+      });
+  },
 };
 
 module.exports = {
