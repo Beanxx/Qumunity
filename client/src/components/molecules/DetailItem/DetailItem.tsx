bcc9b309a04e75824ef60fdc4c830915c806c45d
mini <0miiii@naver.com>
2022-11-27 14:13:22 +0900

    ✨ Feat: detailItem delete handler add

--- client/src/components/molecules/DetailItem/DetailItem.tsx
+++ client/src/components/molecules/DetailItem/DetailItem.tsx
@@ -1,4 +1,6 @@
 import React from "react"
+import axios from "axios"
+import { useNavigate } from "react-router"
 import * as S from "./DetailItem.styles"
 import Tag from "../../atoms/Tag/Tag"
 import SmallProfile from "../../atoms/SmallProfile"
@@ -14,6 +16,34 @@ export type Props = {
 }
 
 const DetailItem: React.FC<Props> = ({ detailData, detailType }) => {
+  const navigate = useNavigate()
+  const api = detailType === "question" ? "detail" : "answer"
+
+  const deleteHandler = async (paramApi: string) => {
+    try {
+      if (window.confirm("삭제하시겠습니까?")) {
+        const response = await axios.delete(
+          `${process.env.REACT_APP_API_URL}/api/${paramApi}/delete`,
+          {
+            data: detailData,
+          }
+        )
+        if (response.data.success === true) {
+          alert("삭제완료")
+          if (detailType === "question") {
+            navigate("/")
+          } else {
+            window.location.reload()
+          }
+        }
+      } else {
+        alert("취소")
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <S.Container detailType={detailType}>
       <S.Side>
@@ -38,6 +68,9 @@ const DetailItem: React.FC<Props> = ({ detailData, detailType }) => {
         ) : null}
         <S.Edit>
           <a href="/">Edit</a>
+          <button type="button" onClick={() => deleteHandler(api)}>
+            Delete
+          </button>
           <a href="/">Delete</a>
           <SmallProfile
             profileImg={`${process.env.PUBLIC_URL}/logo192.png`}
