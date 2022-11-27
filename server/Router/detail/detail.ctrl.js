e89702138fc916ddb1febdc20671235c513acb5a
mini <0miiii@naver.com>
2022-11-27 12:25:33 +0900

    ✨ feat: server detail delete ctrl

--- server/Router/detail/detail.ctrl.js
+++ server/Router/detail/detail.ctrl.js
@@ -8,14 +8,28 @@ const output = {
       const detailData = await Post.findOne({ postNum: req.params.id })
         .populate("author")
         .exec();
-      res.status(200).json(detailData);
+      return res.status(200).json(detailData);
     } catch (err) {
-      res.status(400).json({ success: false, msg: err });
+      return res.status(400).json({ success: false, msg: err });
     }
   },
 };
 
-const process = {};
+const process = {
+  edit: async (req, res) => {
+    try {
+    } catch (err) {}
+  },
+  delete: async (req, res) => {
+    try {
+      console.log(req.body);
+      await Post.deleteOne({ _id: req.body._id }).exec();
+      return res.status(200).json({ success: true });
+    } catch (err) {
+      return res.status(400).json({ success: false, msg: err });
+    }
+  },
+};
 
 module.exports = {
   output,
