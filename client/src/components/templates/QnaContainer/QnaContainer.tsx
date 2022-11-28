efe4ea2f41945373e71c2ea332d3f3f1ecd88594
Beanxx <tnqls991211@naver.com>
2022-11-29 02:43:48 +0900

    ✨ feat: QnaContainer - type & props 추가

--- client/src/components/templates/QnaContainer/QnaContainer.tsx
+++ client/src/components/templates/QnaContainer/QnaContainer.tsx
@@ -1,16 +1,29 @@
 import React from "react"
+import MyQuestionList from "../../organisms/MyQuestionList/MyQuestionList"
 import * as S from "./styles"
+import { postType } from "../../../types/post.interface"
+import answerType from "../../../types/answer.interface"
+import MyAnswerList from "../../organisms/MyAnswerList/MyAnswerList"
 
-const QnaContainer = () => {
+interface Props {
+  myQuestionData: postType[]
+  myAnswerData: answerType[]
+}
+
+const QnaContainer: React.FC<Props> = ({ myQuestionData, myAnswerData }) => {
   return (
     <S.Container>
       <div className="inner_box">
         <div className="title_style">Questions</div>
-        <S.ListBox />
+        <S.ListBox>
+          <MyQuestionList myQuestionData={myQuestionData} />
+        </S.ListBox>
       </div>
       <div className="inner_box">
         <div className="title_style">Answers</div>
-        <S.ListBox />
+        <S.ListBox>
+          <MyAnswerList myAnswerData={myAnswerData} />
+        </S.ListBox>
       </div>
     </S.Container>
   )
