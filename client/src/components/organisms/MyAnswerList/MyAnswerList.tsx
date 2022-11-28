7e3bf8c524fd5d80c44306d861c9bfb453958d51
Beanxx <tnqls991211@naver.com>
2022-11-29 02:43:23 +0900

    ✨ feat: MyAnswerList - 내가 작성한 답변 리스트 추가

---
+++ client/src/components/organisms/MyAnswerList/MyAnswerList.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import answerType from "../../../types/answer.interface"
+import MyAnswerItem from "../../molecules/MyAnswerItem/MyAnswerItem"
+
+interface Props {
+  myAnswerData: answerType[]
+}
+
+const MyAnswerList: React.FC<Props> = ({ myAnswerData }) => {
+  console.log(myAnswerData)
+  return (
+    <ul>
+      {myAnswerData.map((el) => (
+        <MyAnswerItem key={el._id} data={el} />
+      ))}
+    </ul>
+  )
+}
+
+export default MyAnswerList
