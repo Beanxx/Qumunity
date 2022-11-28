aa0dc42b4143506373b1474832b9be34889cef0d
Beanxx <tnqls991211@naver.com>
2022-11-29 02:42:37 +0900

    ✨ feat: MyAnswerItem - 내가 작성한 답변 아이템 추가

---
+++ client/src/components/molecules/MyAnswerItem/MyAnswerItem.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import moment from "moment"
+import * as S from "./styles"
+import answerType from "../../../types/answer.interface"
+
+interface Props {
+  data: answerType
+}
+
+const MyQuestionItem: React.FC<Props> = ({ data }) => {
+  const date = moment(data.createdDate).format("YYYY MM/DD, HH:mm")
+
+  return (
+    <S.Container>
+      <S.TopBox>
+        <div>{data.votes} votes</div>
+      </S.TopBox>
+      <S.TitleBox>{data.content}</S.TitleBox>
+      <S.BottomBox>
+        <div>{date}</div>
+      </S.BottomBox>
+    </S.Container>
+  )
+}
+
+export default MyQuestionItem
