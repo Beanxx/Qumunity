86b438fe135493aa4b22a0e4f59723b7be30afd3
mini <0miiii@naver.com>
2022-11-29 18:21:13 +0900

    ✨ Feat: tag update
    Post tags are stored separately in the model

--- server/Router/ask/ask.ctrl.js
+++ server/Router/ask/ask.ctrl.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const Post = require("../../Model/post");
+const Tags = require("../../Model/tags");
 const { User } = require("../../Model/User");
 const { Counter } = require("../../Model/Counter");
 
 const process = {
   register: async (req, res) => {
     const postData = new Post(req.body);
+    const tags = req.body.tags;
     try {
       const userInfo = await User.findOne({ uid: req.body.uid }).exec();
       postData.author = userInfo._id;
@@ -14,6 +16,15 @@ const process = {
       postData.postNum = count.postNum;
       await postData.save();
       await Counter.updateOne({ name: "counter" }, { $inc: { postNum: 1 } });
+
+      tags.map(async (el) => {
+        await Tags.findOneAndUpdate(
+          { tagName: el },
+          { $inc: { tagCount: 1 } },
+          { upsert: true }
+        ).exec();
+      });
+
       return res.status(200).json({ success: true });
     } catch (err) {
       return res.status(400).json({ success: false, msg: err });
