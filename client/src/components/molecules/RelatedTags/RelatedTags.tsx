7c3f9df3ef8b96fdef35e5e0d07575f8643c36bc
mini <0miiii@naver.com>
2022-11-29 18:41:11 +0900

    ✨ Feat: related tags axios

--- client/src/components/molecules/RelatedTags/RelatedTags.tsx
+++ client/src/components/molecules/RelatedTags/RelatedTags.tsx
@@ -1,23 +1,45 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 import Tag from "../../atoms/Tag/Tag"
 import * as S from "./RelatedTags.styles"
 
+interface tagType {
+  _id: string
+  tagName: string
+  tagCount: number
+}
+
 const RelatedTags = () => {
+  const [tagList, setTagList] = useState<tagType[]>([])
+
+  const getTagData = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/main/tags`
+      )
+      setTagList(data)
+    } catch (err) {
+      // 임시에러처리
+      // eslint-disable-next-line no-alert
+      alert(err)
+    }
+  }
+
+  useEffect(() => {
+    getTagData()
+  }, [])
+
+  console.log(tagList)
+
   return (
     <S.Container>
       <h3>Related Tags</h3>
-      <li>
-        <Tag>javascript</Tag>
-        <S.Num> x 241552</S.Num>
-      </li>
-      <li>
-        <Tag>typescript</Tag>
-        <S.Num> x 241552</S.Num>
-      </li>
-      <li>
-        <Tag>React</Tag>
-        <S.Num> x 241552</S.Num>
-      </li>
+      {tagList.map((el) => (
+        <li key={el._id}>
+          <Tag>{el.tagName}</Tag>
+          <S.Num> x {el.tagCount}</S.Num>
+        </li>
+      ))}
     </S.Container>
   )
 }
