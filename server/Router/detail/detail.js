1b93e91b48d3a6da706c22d7903dfce0ab652c01
mini <0miiii@naver.com>
2022-12-03 18:42:49 +0900

    ✨ Feat: Add router likes and dislikes

--- server/Router/detail/detail.js
+++ server/Router/detail/detail.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const ctrl = require("./detail.ctrl");
 
 router.get("/:id", ctrl.output.detail);
-
 router.get("/views/:id", ctrl.process.view);
+
+router.put("/votes/like", ctrl.process.like);
+router.put("/votes/dislike", ctrl.process.dislike);
 
 router.delete("/delete", ctrl.process.delete);
 
