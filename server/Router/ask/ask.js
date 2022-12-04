0ac2112968a70213c998c9c9c36528327f1077d9
mini <0miiii@naver.com>
2022-12-05 03:42:27 +0900

    🔥 Remove: edit router
    Move edit process to ask ctrl

--- server/Router/ask/ask.js
+++ server/Router/ask/ask.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const ctrl = require("./ask.ctrl");
 
 router.post("/register", ctrl.process.register);
+router.post("/edit", ctrl.process.edit);
 
 module.exports = router;
