f0fdab298b9e9066d63689234de0cb5a8eef4ad2
mini <0miiii@naver.com>
2022-12-05 03:51:25 +0900

    ✨ Feat: add axios, props
    Add props, api for fix

--- client/src/components/templates/AskForms/AskForms.tsx
+++ client/src/components/templates/AskForms/AskForms.tsx
@@ -1,11 +1,12 @@
-import React, { useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import axios from "axios"
 import { useSelector } from "react-redux"
-import { useNavigate } from "react-router"
+import { useNavigate, useParams } from "react-router"
 import { Editor } from "@toast-ui/react-editor"
 import { RootState } from "../../../redux/store"
 import AskForm from "../../organisms/AskForm/AskForm"
 import Button from "../../atoms/Button"
+import { postType } from "../../../types/post.interface"
 import Container from "./AskForms.styles"
 
 type Refs = HTMLInputElement & HTMLTextAreaElement & Editor
@@ -17,11 +18,34 @@ const AskForms = () => {
   const summaryInputRef = useRef<Refs>(null)
   const contentEditorRef = useRef<Refs>(null)
   const uid = useSelector((state: RootState) => state.user.uid)
+  const { id } = useParams()
+  const [editData, setEditData] = useState<postType>()
+  const editor = id ? "edit" : "register"
+
+  const getDetailData = async (postNum: string) => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_URL}/api/detail/${postNum}`
+      )
+      setEditData(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    if (id) {
+      getDetailData(id)
+    }
+  }, [])
 
   const submitHandler = async () => {
     const title = titleInputRef.current?.value
     const summary = summaryInputRef.current?.value
-    const content = contentEditorRef.current?.getInstance().getHTML()
+    const content = {
+      html: contentEditorRef.current?.getInstance().getHTML(),
+      markdown: contentEditorRef.current?.getInstance().getMarkdown(),
+    }
     const postData = {
       uid,
       title,
@@ -29,12 +53,11 @@ const AskForms = () => {
       content,
       tags,
     }
+    console.log(postData)
     const { data } = await axios.post(
-      `${process.env.REACT_APP_API_URL}/api/ask/register`,
+      `${process.env.REACT_APP_API_URL}/api/ask/${editor}`,
       postData
     )
-    console.log(postData)
-
     if (data.success) {
       navigate("/")
     } else {
@@ -44,7 +67,13 @@ const AskForms = () => {
 
   return (
     <Container>
-      <AskForm id="title" title="Title" type="default" ref={titleInputRef}>
+      <AskForm
+        id="title"
+        title="Title"
+        type="default"
+        ref={titleInputRef}
+        value={editData?.title}
+      >
         Be specific and imagine you’re asking a question to another person.
       </AskForm>
 
@@ -53,6 +82,7 @@ const AskForms = () => {
         title="What are the details of your problem?"
         type="textarea"
         ref={summaryInputRef}
+        value={editData?.summary}
       >
         Introduce the problem and expand on what you put in the title. Minimum
         20 characters.
@@ -61,14 +91,21 @@ const AskForms = () => {
       <AskForm
         id="content"
         title="What did you try and what were you expecting?"
-        type="editor"
+        type={editor}
         ref={contentEditorRef}
+        value={editData?.content.markdown}
       >
         Describe what you tried, what you expected to happen, and what actually
         resulted. Minimum 20 characters.
       </AskForm>
 
-      <AskForm id="tags" title="Tags" type="tagsInput" onEnterTag={setTags}>
+      <AskForm
+        id="tags"
+        title="Tags"
+        type="tagsInput"
+        onEnterTag={setTags}
+        value={editData?.tags}
+      >
         Add up to 5 tags to describe what your question is about. Start typing
         to see suggestions.
       </AskForm>
