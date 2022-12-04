dfd92ace23804b98d4a0dfd119459a35f03722ec
mini <0miiii@naver.com>
2022-12-04 22:21:15 +0900

    ✨ Feat: Change from textarea to editor

--- client/src/components/templates/DetailContent/DetailContent.tsx
+++ client/src/components/templates/DetailContent/DetailContent.tsx
@@ -2,14 +2,15 @@ import React, { FormEvent, useEffect, useRef, useState } from "react"
 import { useParams } from "react-router"
 import axios from "axios"
 import { format } from "timeago.js"
+import { Editor } from "@toast-ui/react-editor"
 import { useSelector } from "react-redux"
 import { RootState } from "../../../redux/store"
 import * as S from "./DetailContent.styles"
 import MiniHeader from "../../molecules/MiniHeader/MiniHeader"
 import AnswerList from "../../organisms/AnswerList/AnswerList"
 import DetailItem from "../../molecules/DetailItem/DetailItem"
+import EditorInput from "../../atoms/EditorInput/EditorInput"
 import Button from "../../atoms/Button"
-import TextArea from "../../atoms/TextArea/TextArea"
 import { postType } from "../../../types/post.interface"
 import answerType from "../../../types/answer.interface"
 
@@ -18,12 +19,13 @@ const DetailContent: React.FC = () => {
   const [answerData, setDataAnswer] = useState<answerType[]>([])
   const { id } = useParams()
   const uid = useSelector((state: RootState) => state.user.uid)
-  const answerInputRef = useRef<HTMLTextAreaElement>(null)
   const createdAt = format(detailData[0]?.createdAt, "en_US")
+
+  const EditorRef = useRef<Editor>(null)
 
   const submitHandler = (event: FormEvent) => {
     event.preventDefault()
-    const answerInput = answerInputRef.current?.value
+    const answerInput = EditorRef.current?.getInstance().getHTML()
     const answer = { uid, content: answerInput, postId: detailData[0]._id }
 
     axios
@@ -85,9 +87,10 @@ const DetailContent: React.FC = () => {
       </ul>
       <S.AnswerTitle>{detailData[0]?.answers} Answer</S.AnswerTitle>
       <AnswerList answerData={answerData} />
+
       <S.AnswerForm onSubmit={submitHandler}>
         <div>Your Answer</div>
-        <TextArea id="answer" name="answer" ref={answerInputRef} />
+        <EditorInput ref={EditorRef} />
         <Button btnType="highlighted" width="auto">
           Post Your Answer
         </Button>
