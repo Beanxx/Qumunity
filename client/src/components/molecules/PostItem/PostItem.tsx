4ba7e171afcc29da2a7b7a970d934a7d06c05f65
mini <0miiii@naver.com>
2022-12-04 17:33:07 +0900

    ✨ Feat: Set relative date

--- client/src/components/molecules/PostItem/PostItem.tsx
+++ client/src/components/molecules/PostItem/PostItem.tsx
@@ -1,17 +1,14 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import axios from "axios"
+import { format } from "timeago.js"
 import * as S from "./PostItem.styles"
 import Tag from "../../atoms/Tag/Tag"
 import SmallProfile from "../../atoms/SmallProfile"
 import { postType } from "../../../types/post.interface"
 
 const PostItem: React.FC<{ postData: postType }> = ({ postData }) => {
-  // const formatter = new Intl.RelativeTimeFormat("ko", { numeric: "auto" })
-  // const start = new Date(postData.createdDate)
-  // const today = new Date()
-  // const daysPassed = Math.ceil((start - today) / (1000 * 60 * 60 * 24))
-  // formatter.format(daysPassed, "hour")
+  const createdAt = format(postData.createdAt, "en_US")
 
   const viewsHandler = async () => {
     await axios.get(
@@ -42,7 +39,7 @@ const PostItem: React.FC<{ postData: postType }> = ({ postData }) => {
           <SmallProfile
             profileImg={postData.author.photoURL}
             userName={postData.author.displayName}
-            createdDate={postData.createdAt}
+            createdDate={createdAt}
           />
         </S.ContentBot>
       </S.Content>
