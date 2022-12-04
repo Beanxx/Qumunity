7ea5d5815676a05427f32d6106b94fe180dd56ae
mini <0miiii@naver.com>
2022-12-04 19:27:36 +0900

    ✨ Feat: activate tab

--- client/src/components/templates/SideBar/SideBar.tsx
+++ client/src/components/templates/SideBar/SideBar.tsx
@@ -1,16 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Container from "./SideBar.styles"
 
 const SideBar = () => {
+  const [activity, setActivity] = useState(0)
+  const tab = [
+    { title: "Question", to: "/" },
+    { title: "Users", to: "/users" },
+  ]
+
+  const tabHandler = (idx: number) => {
+    setActivity(idx)
+  }
+
   return (
     <Container>
-      <li>
-        <Link to="/">Question</Link>
-      </li>
-      <li className="on">
-        <Link to="/users">Users</Link>
-      </li>
+      {tab.map((el, idx) => {
+        return (
+          <li key={el.title} className={activity === idx ? "on" : ""}>
+            <Link to={el.to} onClick={() => tabHandler(idx)}>
+              {el.title}
+            </Link>
+          </li>
+        )
+      })}
     </Container>
   )
 }
