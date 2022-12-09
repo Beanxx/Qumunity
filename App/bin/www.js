880cb75689e7b3057948bcd4bd91a2a4e7424d40
Beanxx <tnqls991211@naver.com>
2022-12-09 22:35:27 +0900

    🐛 fix: bin/www.js - config file 변경

--- App/bin/www.js
+++ App/bin/www.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 const app = require("../index");
-const config = require("./server/config/key.js");
+const config = require("../server/config/key.js");
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
