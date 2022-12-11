1d1b9022a148c2b8f880a4c190e9110a191d4f94
Beanxx <tnqls991211@naver.com>
2022-12-11 17:05:24 +0900

    💄 design: GlobalStyle - 색상값 변수 지정

--- App/client/src/GlobalStyle.ts
+++ App/client/src/GlobalStyle.ts
@@ -4,18 +4,35 @@ import reset from "styled-reset"
 const GlobalStyle = createGlobalStyle`
   ${reset}
 
+
+  :root {
+    --header-color: #161B22;
+    --main-bgc: #0d1117;
+    --font-color: #f0f6fc;
+    --text-color: #c9d1d9;
+    --title-color: #58a6ff;
+    --content-color: #8b949e;
+    --btn-color: #238636;
+    --side-bgc: #161b22;
+    --border-color: #21262d;
+    --sub-color: #58a6ff;
+  }
+
   body {
     padding-top: 53px;
+    background-color: var(--main-bgc);
+    color: var(--font-color)
   }
 
   * {
     box-sizing: border-box;
     line-height: 1.2;
+    
   }
 
   a {
     text-decoration: none;
-    color: black;
+    color: #fff;
   }
 `
 
